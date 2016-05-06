e1bda7cb5a9e1223b3341217c13fa1ee343d5e57
isaev-bolsen <al_mamistov@mail.ru>
2016-05-06 19:35:11 +0300

    AS is

---
+++ NamingHelper/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ClassGenerator.Host
+{
+    public static class Helper
+    {
+        private static string MoveDigitsFromHeadToTail(string text)
+        {
+            var numbPrefix = text.TakeWhile(c => char.IsDigit(c));
+            int DigitsCount = numbPrefix.Count();
+            if (DigitsCount == 0) return text;
+            else return text.Substring(DigitsCount) + "_" + new string(numbPrefix.ToArray());
+        }
+
+        internal static string StartWithUpperCase(string text)
+        {
+            if (text.Length < 2) return text.ToUpper();
+            else return text.Substring(0, 1).ToUpper() + text.Substring(1);
+        }
+
+        public static string ConvertToValidCSName(string text)
+        {
+            return StartWithUpperCase(MoveDigitsFromHeadToTail(ReplaceChars(text)));
+        }
+
+        private static Regex ReplaceTo_ = new Regex("[-/]+", RegexOptions.Compiled); //two symbols '-' and '/'
+        private static Regex ReplaceToEmpty = new Regex("\\W+", RegexOptions.Compiled); //Symbols not for Name
+
+        private static string ReplaceChars(string text)
+        {
+            return ReplaceToEmpty.Replace(ReplaceTo_.Replace(text.Split('.').Last(), "_").Replace("=", "Equal"), string.Empty);
+        }
+    }
+}
---
+++ UnitTestProject1/HelperTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassGenerator.Host
+{
+    [TestClass]
+    public class HelperTest
+    {
+        [TestMethod]
+        public void ConvertToValidNameTest()
+        {
+            Assert.AreEqual("Zloy123Admin_456", Helper.ConvertToValidCSName("456Zloy123Admin"));
+        }
+    }
+}
