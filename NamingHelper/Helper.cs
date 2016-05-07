f464c8e67392d40f03d5ae4cdc3da084953305b4
isaev-bolsen <al_mamistov@mail.ru>
2016-05-07 11:45:51 +0300

    Order

--- NamingHelper/Helper.cs
+++ NamingHelper/Helper.cs
@@ -9,6 +9,11 @@ namespace ClassGenerator.Host
         private static Regex ReplaceToEmpty = new Regex("\\W+", RegexOptions.Compiled); //Symbols not for Name
         private static Regex LeadingNumbers = new Regex("\\A[0-9]+", RegexOptions.Compiled); //Leading Numbers
 
+        private static string ReplaceChars(string text)
+        {
+            return ReplaceToEmpty.Replace(ReplaceTo_.Replace(text, "_").Replace("=", "Equal"), string.Empty);
+        }
+
         private static string MoveDigitsFromHeadToTail(string text)
         {
             var Match = LeadingNumbers.Match(text);
@@ -16,7 +21,7 @@ namespace ClassGenerator.Host
             else return text.Substring(Match.Length) + "_" + Match.Value;
         }
 
-        internal static string StartWithUpperCase(string text)
+        public static string StartWithUpperCase(string text)
         {
             if (text.Length < 2) return text.ToUpper();
             else return text.Substring(0, 1).ToUpper() + text.Substring(1);
@@ -25,11 +30,6 @@ namespace ClassGenerator.Host
         public static string ConvertToValidCSName(string text)
         {
             return StartWithUpperCase(MoveDigitsFromHeadToTail(ReplaceChars(text.Split('.').Last())));
-        }
-
-        private static string ReplaceChars(string text)
-        {
-            return ReplaceToEmpty.Replace(ReplaceTo_.Replace(text, "_").Replace("=", "Equal"), string.Empty);
         }
     }
 }
